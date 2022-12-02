8acbf6c8358b81045947cd68900e5ecb42d29e3f
Alan <alanhaberapp@pm.me>
2022-12-02 17:31:24 +0100

    footer change

--- frontend/src/components/layout/TheFooter.vue
+++ frontend/src/components/layout/TheFooter.vue
@@ -1,5 +1,5 @@
 <template>
   <v-footer app :absolute="true" class="d-flex justify-center" color="app1">
-    <v-btn class="text-overline" href="https://github.com/haberalan" variant="plain">&copy; Alan Haber 2022</v-btn>
+    <v-btn class="text-overline" href="https://github.com/haberalan" variant="plain">&copy; 2022 Alan Haber</v-btn>
   </v-footer>
 </template>
