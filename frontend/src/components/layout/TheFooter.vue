7468fa720373cd8a1e5e595d35d9f9436a229c6a
Alan Haber <alanhaberwork@pm.me>
2023-01-10 15:11:36 +0100

    Updated date

--- frontend/src/components/layout/TheFooter.vue
+++ frontend/src/components/layout/TheFooter.vue
@@ -1,5 +1,5 @@
 <template>
   <v-footer app :absolute="true" class="d-flex justify-center" color="app1">
-    <v-btn class="text-overline" href="https://github.com/haberalan" variant="plain">&copy; 2022 Alan Haber</v-btn>
+    <v-btn class="text-overline" href="https://github.com/haberalan" variant="plain">&copy; {{ new Date().getFullYear() }} Alan Haber</v-btn>
   </v-footer>
 </template>
